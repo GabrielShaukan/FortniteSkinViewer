fc7dda8f8439abfbe0dfd6c608f2ea28d568e917
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-05-24 12:38:45 +0200

    Added skins activity and button functionality

---
+++ app/src/main/java/com/example/gabriel/fortniteskinviewer/DefaultSkins.java
@@ -0,0 +1,13 @@
+package com.example.gabriel.fortniteskinviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DefaultSkins extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default_skins);
+    }
+}

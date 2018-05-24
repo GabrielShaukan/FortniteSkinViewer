60255084ac7e48ffd84d4bd017617724788e3218
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-05-24 12:20:18 +0200

    Added skins activity and button functionality

--- app/src/main/java/com/example/gabriel/fortniteskinviewer/MainActivity.java
+++ app/src/main/java/com/example/gabriel/fortniteskinviewer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gabriel.fortniteskinviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSkins = (Button) findViewById(R.id.btnSkins);
+
+        btnSkins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySkins();
+            }
+        });
+    }
+
+    void openActivitySkins () {
+        Intent intent = new Intent (this, Skins.class);
+        startActivity(intent);
     }
 }
